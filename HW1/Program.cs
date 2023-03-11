e0ab1378fe142bb02394d64e1adc7789b0a70113
Игорь Павленко <illusiopard@gmail.com>
2023-03-12 02:44:42 +0300

    Добавьте файлы проекта.

---
+++ HW1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace Module13.HW1
+{
+    internal class Program
+    {
+        static List<string> textList = new List<string>();
+        static LinkedList<string> textLinkedList = new LinkedList<string>();
+        static void Main(string[] args)
+        {
+            var txtWatch = new Stopwatch();
+
+            string[] path = { @"d:\", "для SF", "Text1hw1_13.txt" };
+            string filePath = Path.Combine(path);
+
+            //// читаем весь файл с рабочего стола в строку текста
+            //string text = File.ReadAllText(filePath);
+
+            string str = string.Empty;
+
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                txtWatch.Start();
+
+                while ((str = sr.ReadLine()) != null)
+                {
+                    textList.Add(str);
+                }
+                txtWatch.Stop(); //имеет значение, где установлен .Stop()
+                sr.Close();
+            }
+            Console.WriteLine($"время потрачено для добавления в List: {txtWatch.ElapsedMilliseconds} ms.");
+
+            txtWatch.Restart();
+
+            str = string.Empty;
+            using (StreamReader sr2 = File.OpenText(filePath))
+            {
+                txtWatch.Start();
+                while ((str = sr2.ReadLine()) != null)
+                {
+                    textLinkedList.AddLast(str);
+                }
+                txtWatch.Stop();
+                sr2.Close();
+            }
+            Console.WriteLine($"время потрачено для добавления в LinkedList: {txtWatch.ElapsedMilliseconds} ms.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace HW2
+{
+    internal class Program
+    {
+        public static Dictionary<string, int> dictionary = new Dictionary<string, int>();
+        static void Main(string[] args)
+        {
+            string[] path = { @"d:\", "для SF", "Text_HW2.txt" };
+            string filePath = Path.Combine(path);
+            string str = string.Empty;
+ 
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                str = sr.ReadToEnd().Replace("-", string.Empty);
+                //Для того чтобы убрать из текста знаки пунктуации используем следующий фрагмент кода: 
+                var noPunctuationText = new string(str.Where(c => !char.IsPunctuation(c)).ToArray());
+
+                // Сохраняем символы-разделители в массив
+                char[] delimiters = new char[] { ' ', '\r', '\n'};
+
+                //создание массива из слов с разбитием строки из текста, удаляя ранее перечисленные символы-разделители
+                var words = str.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    if (!dictionary.ContainsKey(word))//проверка есть ли в dictionary слово с заданными Key и Value
+                    {
+                        dictionary.Add(word, 1);       //если нет - записываем
+                    }
+                    else
+                    {
+                        dictionary[word]++;             //если есть - увеличиваем Value на 1
+                    }
+                }
+                //отсортированный список
+                //var sortedDictionary = dictionary.OrderByDescending(x => x.Value).Take(10); //сортировка по значению по убыванию
+                                                                                           //значения.оставляютмя 10 первых значения
+                foreach (var item in dictionary.OrderByDescending(x => x.Value).Take(10))
+                {
+                    Console.WriteLine(item);
+                }
+                sr.Close();
+            }
+            Console.ReadKey();
+        }
+    }
+}
